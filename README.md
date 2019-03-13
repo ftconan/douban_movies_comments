2e41e09065d21d1d8b9919680a362168fb904150
magician <1508907793@qq.com>
2019-03-13 10:11:50 +0800

    CNN,RNN,RNN GRU豆瓣影评分类器完成

--- README.md
+++
@@ -1,5 +0,0 @@
-# douban_movies_comments
-douban_movies_comments(豆瓣影评情感分析)
-
-### 豆瓣影评爬虫
-1. scrapy + phantom.js
--- douban_movie2/analysis_comment.py
+++ douban_movie2/analysis_comment.py
@@ -324,7 +324,7 @@ def cnn_model(features, target):
         # 第二卷积层
         conv2 = tf.contrib.layers.convolution2d(pool1,
                                                 N_FILTERS,
-                                                FILTER_SHAPE1,
+                                                FILTER_SHAPE2,
                                                 padding='VALID')
         # 抽取特征
         pool2 = tf.squeeze(tf.reduce_max(conv2, 1), squeeze_dims=[1])
@@ -349,7 +349,7 @@ vocab_processor = learn.preprocessing.VocabularyProcessor(MAX_DOCUMENT_LENGTH, m
 x_train = np.array(list(vocab_processor.fit_transform(train_data)))
 x_test = np.array(list(vocab_processor.transform(test_data)))
 n_words = len(vocab_processor.vocabulary_)
-print('Total words:%d' % n_words)
+print('CNN Total words:%d' % n_words)
 
 cate_dic = {'like': 1, 'nlike': 0}
 y_train = pd.Series(train_target).apply(lambda x:cate_dic[x], train_target)
@@ -364,103 +364,122 @@ y_predicted = classifier.predict(x_test)['class']
 score = metrics.accuracy_score(y_test, y_predicted)
 print('CNN Accuracy:{0:f}'.format(score))
 
-# # 使用RNN完成文本分类
-# learn = tf.contrib.learn
-# FLAGS = None
-# MAX_DOCUMENT_LENGTH = 15
-# MIN_WORD_FREQUENCE = 1
+# 使用RNN完成文本分类
+learn = tf.contrib.learn
+FLAGS = None
+MAX_DOCUMENT_LENGTH = 15
+MIN_WORD_FREQUENCE = 1
+EMBEDDING_SIZE = 50
 
 # 处理词汇
-# vocab_processor = learn.preprocessing.VocabularyProcessor(MAX_DOCUMENT_LENGTH,
-#                                                           min_frequency=MIN_WORD_FREQUENCE)
-# x_train = np.array(list(vocab_processor.fit_transform(train_data)))
-# x_test = np.array(list(vocab_processor.transform(test_data)))
-# print('Total words: %d' % n_words)
-#
-#
-# def bag_of_words_model(features, target):
-#     """
-#     先转成词袋模型
-#     :param features:
-#     :param target:
-#     :return:
-#     """
-#     target = tf.one_hot(target, 15, 1, 0)
-#     features = encoders.bow_encoder(features,
-#                                     vocab_size=n_words,
-#                                     embed_dim=EMBEDDING_SIZE)
-#     logits = tf.contrib.layers.fully_connected(features,
-#                                                15,
-#                                                activation_fn=None)
-#     loss = tf.contrib.losses.softmax_cross_entropy(logits, target)
-#     train_op = tf.contrib.layers.optimize_loss(loss,
-#                                                tf.contrib.framework.get_global_step(),
-#                                                optimizer='Adam',
-#                                                learning_rate=0.01)
-#
-#     return ({
-#                 'class': tf.argmax(logits, 1),
-#                 'prob': tf.nn.softmax(logits)
-#             }, loss, train_op)
+vocab_processor = learn.preprocessing.VocabularyProcessor(MAX_DOCUMENT_LENGTH,
+                                                          min_frequency=MIN_WORD_FREQUENCE)
+x_train = np.array(list(vocab_processor.fit_transform(train_data)))
+x_test = np.array(list(vocab_processor.transform(test_data)))
+n_words = len(vocab_processor.vocabulary_)
+print('RNN Total words: %d' % n_words)
 
-#
-# model_fn = bag_of_words_model
-# classifier = learn.SKCompat(learn.Estimator(model_fn=model_fn))
-#
-# # 5.测试和预测
-# classifier.fit(x_train, y_train, steps=1000)
-# y_predicted = classifier.predict(x_test)['class']
-# score = metrics.accuracy_score(y_test, y_predicted)
-# print('Accuracy: {0:f}'.format(score))
 
-# def rnn_model(features, target):
-#     """
-#     用RNN模型（这里用的是GRU）完成文本分类
-#     """
-#     # Convert indexes of words into embeddings.
-#     # This creates embeddings matrix of [n_words, EMBEDDING_SIZE] and then
-#     # maps word indexes of the sequence into [batch_size,sequence_length,
-#     # EMBEDDING_SIZE].
-#     word_vectors = tf.contrib.layers.embed_sequence(features
-#                                                     , vocab_size=n_words
-#                                                     , embed_dim=EMBEDDING_SIZE
-#                                                     , scope='words')
-#     # Split into list of embedding per word, while removing doc length dim。
-#     # word_list results to be a list of tensors [batch_size,EMBEDDING_SIZE].
-#     word_list = tf.unstack(word_vectors, axis=1)
-#
-#     # Create a Gated Recurrent Unit cell with hidden size of EMBEDDING_SIZE.
-#     cell = tf.contrib.rnn.GRUCell(EMBEDDING_SIZE)
-#
-#     # Create an unrolled Recurrent Neural Networks to length of
-#     # MAX_DOCUMENT_LENGTH and passes word_list as inputs for each unit.
-#     _, encoding = tf.contrib.rnn.static_rnn(cell, word_list, dtype=tf.float32)
-#
-#     # Given encoding of RNN, take encoding of last step (e.g hidden size of the
-#     # neural network of last step) and pass it as features for logistic
-#     # regression over output classes.
-#     target = tf.one_hot(target, 15, 1, 0)
-#     logits = tf.contrib.layers.fully_connected(encoding, 15, activation_fn=None)
-#     loss = tf.contrib.losses.softmax_cross_entropy(logits, target)
-#
-#     # Create a training op.
-#     train_op = tf.contrib.layers.optimize_loss(
-#         loss,
-#         tf.contrib.framework.get_global_step(),
-#         optimizer='Adam',
-#         learning_rate=0.01)
-#
-#     return ({
-#                 'class': tf.argmax(logits, 1),
-#                 'prob': tf.nn.softmax(logits)
-#             }, loss, train_op)
+def bag_of_words_model(features, target):
+    """
+    先转成词袋模型
+    :param features:
+    :param target:
+    :return:
+    """
+    target = tf.one_hot(target, 15, 1, 0)
+    features = encoders.bow_encoder(features,
+                                    vocab_size=n_words,
+                                    embed_dim=EMBEDDING_SIZE)
+    logits = tf.contrib.layers.fully_connected(features,
+                                               15,
+                                               activation_fn=None)
+    loss = tf.contrib.losses.softmax_cross_entropy(logits, target)
+    train_op = tf.contrib.layers.optimize_loss(loss,
+                                               tf.contrib.framework.get_global_step(),
+                                               optimizer='Adam',
+                                               learning_rate=0.01)
 
-#
-# model_fn = rnn_model
-# classifier = learn.SKCompat(learn.Estimator(model_fn=model_fn))
-#
-# # Train and predict
-# classifier.fit(x_train, y_train, steps=1000)
-# y_predicted = classifier.predict(x_test)['class']
-# score = metrics.accuracy_score(y_test, y_predicted)
-# print('Accuracy:{0:f}'.format(score))
+    return ({
+                'class': tf.argmax(logits, 1),
+                'prob': tf.nn.softmax(logits)
+            }, loss, train_op)
+
+
+model_fn = bag_of_words_model
+classifier = learn.SKCompat(learn.Estimator(model_fn=model_fn))
+
+# 5.测试和预测
+classifier.fit(x_train, y_train, steps=1000)
+y_predicted = classifier.predict(x_test)['class']
+score = metrics.accuracy_score(y_test, y_predicted)
+print('RNN Accuracy: {0:f}'.format(score))
+
+
+def rnn_model(features, target):
+    """
+    用RNN模型（这里用的是GRU）完成文本分类
+    """
+    # Convert indexes of words into embeddings.
+    # This creates embeddings matrix of [n_words, EMBEDDING_SIZE] and then
+    # maps word indexes of the sequence into [batch_size,sequence_length,
+    # EMBEDDING_SIZE].
+    word_vectors = tf.contrib.layers.embed_sequence(features
+                                                    , vocab_size=n_words
+                                                    , embed_dim=EMBEDDING_SIZE
+                                                    , scope='words')
+    # Split into list of embedding per word, while removing doc length dim。
+    # word_list results to be a list of tensors [batch_size,EMBEDDING_SIZE].
+    word_list = tf.unstack(word_vectors, axis=1)
+
+    # Create a Gated Recurrent Unit cell with hidden size of EMBEDDING_SIZE.
+    cell = tf.contrib.rnn.GRUCell(EMBEDDING_SIZE)
+
+    # Create an unrolled Recurrent Neural Networks to length of
+    # MAX_DOCUMENT_LENGTH and passes word_list as inputs for each unit.
+    _, encoding = tf.contrib.rnn.static_rnn(cell, word_list, dtype=tf.float32)
+
+    # Given encoding of RNN, take encoding of last step (e.g hidden size of the
+    # neural network of last step) and pass it as features for logistic
+    # regression over output classes.
+    target = tf.one_hot(target, 15, 1, 0)
+    logits = tf.contrib.layers.fully_connected(encoding, 15, activation_fn=None)
+    loss = tf.contrib.losses.softmax_cross_entropy(logits, target)
+
+    # Create a training op.
+    train_op = tf.contrib.layers.optimize_loss(
+        loss,
+        tf.contrib.framework.get_global_step(),
+        optimizer='Adam',
+        learning_rate=0.01)
+
+    return ({
+                'class': tf.argmax(logits, 1),
+                'prob': tf.nn.softmax(logits)
+            }, loss, train_op)
+
+
+model_fn = rnn_model
+classifier = learn.SKCompat(learn.Estimator(model_fn=model_fn))
+
+# Train and predict
+classifier.fit(x_train, y_train, steps=1000)
+y_predicted = classifier.predict(x_test)['class']
+score = metrics.accuracy_score(y_test, y_predicted)
+print('RNN GRU Accuracy:{0:f}'.format(score))
+
+
+def pred(commment):
+    sentences = []
+    preprocess_text([commment], sentences, 'unknown')
+    x, y = zip(*sentences)
+    x_tt = np.array(list(vocab_processor.transform([x[0]])))
+
+    if classifier.predict(x_tt)['class'][0]:
+        print('like')
+    else:
+        print('nlike')
+
+
+pred('好精彩的电影！')
+pred('烂片啊！')
